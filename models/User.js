d2d900cc802a73135f90ade667af4e4cd49b40d9
listeven <st3venL39@gmail.com>
2020-11-26 22:49:10 -0800

    update

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// body object, can add additional properties
 const userSchema = new Schema({
   googleId: String
 });
 
-
-// two arguments mean trying to load something into mongoose 
+// two arguments mean trying to load something into mongoose
+// make new collection called users
 mongoose.model('users', userSchema);
--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,22 @@ const keys = require('../config/keys');
 // one argument means try to fetch something out of mongoose
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  // id is the primary key in the mongoDB
+  // can't always use the googleID, might use LinkedIN for example
+  // user.id is the cookie
+  done(null, user.id);
+});
+
+passport.deserializeUser((user.id, done) => {
+  // turning an id into a mongoose model instance
+  // search collection, call done
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
+
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -14,7 +30,20 @@ passport.use(
     // after permission granted, send them to route above
   },
   (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save();
+    User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser);
+          // we already have a record with given profile ID
+          // if user doesn't exist, returns true else null
+        } else {
+          // we don't have a user record with this ID, make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+          // call to .save, saves model instance to the database
+        }
+      })
     }
   )
 );
